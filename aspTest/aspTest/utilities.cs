d51a965ad21a0eee7d4f8fc68904e5115ed7cd70
benjbaker <benbaker1302@gmail.com>
2015-01-26 11:02:59 +0000

    some work

---
+++ aspTest/aspTest/utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspTest
+{
+    public class utilities
+    {
+        public static int MyFaveNumber = 53;
+
+        public static int giveInt()
+        {
+            return 5;
+        }
+        public static string giveString()
+        {
+            return "hello world";
+        }
+        
+    }
+}
